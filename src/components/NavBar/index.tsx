c3361766fb35c7e35f312bac4cdbb1edd84cfe17
ryanoliveir <ryandehallo229@gmail.com>
2025-02-15 19:13:33 -0300

    add: improve NavBar Styles

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,14 +3,14 @@ import Logo from "../../assets/logo.png";
 function NavBar() {
   return (
     <>
-      <div className="fixed top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm py-4 sm:py-4">
+      <div data-aos="fade-down" className="fixed top-0 right-0 w-full z-[99] bg-black/10 backdrop-blur-sm py-4 sm:py-4">
         <div className="container">
           <div className="flex justify-between">
             <div className="flex items-center gap-4 text-white font-bold text-2xl">
               <img className="w-10" src={Logo} alt="Logo" />
               <span>TCJ-SPACE</span>
             </div>
-            <div className="text-white">
+            <div className="text-white hidden md:block ">
               <ul className="flex items-center gap-6 text-xl py-4 sm:py-0">
                 <li>
                   <a href="#"></a>About
