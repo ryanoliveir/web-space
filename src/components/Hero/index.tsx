6ed3b3a2e377a0484aed1acbb74f909b36e1f7f8
ryanoliveir <ryandehallo229@gmail.com>
2025-02-15 19:41:31 -0300

    add: animation in Hero component with aos module

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,17 +6,20 @@ function Hero() {
         <div className="h-full flex justify-center items-center p-4">
             <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="space-y-4">
-                    <h1 className="text-5xl font-bold uppercase">Orbite The Earth</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    <h1 data-aos="fade-up" className="text-5xl font-bold uppercase">Orbite The Earth</h1>
+                    <p data-aos="fade-up" data-aos-delay="300">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                         Necessitatibus expedita quos error non magnam eos asperiores 
                         quam dignissimos repudiandae, ullam nesciunt eum molestias 
                         quasi vero cupiditate vel natus earum! Dolorum.
                     </p>
-                    <button className="bg-blue-400 text-white hover:bg-blue-500 rounded-md px-4 py-1 duration-1000">Learn more</button>
+                    <button  data-aos="fade-up" data-aos-delay="500" className="primary-button">Learn more</button>
                 </div>
             </div>
         </div>
         <img  className="absolute right-0 bottom-0 brightness-50 z-10" src={MountainPng} alt=""/>
+        <div className="absolute w-full bottom-0 z-30 bg-gradient-to-b from-transparent from-10% to-black to-90% h-[20px] sm:h-[50px] md:h-[60px]">
+
+        </div>
       </div>
     </>
   );
